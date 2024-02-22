cff47e24827ef2a56815ad0b48b907f0f00d3b35
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-02-22 16:38:11 +0530

    02_dataType type conversion

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -5,7 +5,11 @@ foo = "bar"; // foo is now a string
 foo = true; // foo is now a boolean
 
 
-
-
 console.log(typeof null)
 console.log(typeof undefined)
+
+
+let score='45abc';
+let valueInNumber=Number(score) // type Conversion
+console.log(typeof valueInNumber) // Number
+console.log( valueInNumber) //NaN
