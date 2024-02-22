94539628fa97ad3c832ea8a6168cac61e33b2252
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-02-22 16:24:19 +0530

    first commit

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountEmail="sy@sy.com";
+let accountPassword="123456";
+var accountNumber=123466;
+accountCity="jaupur";
+let accountState;
+
+accountPassword='5456465'
+accountNumber=165656
+accountCity='noida'
+
+
+console.table([accountEmail,accountPassword,accountNumber,accountCity,accountState])
