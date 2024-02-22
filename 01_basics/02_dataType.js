f9df4bb0d9e79196083ac999949dd755c7ec256e
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-02-22 16:30:00 +0530

    02_dataType

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -1,2 +1,11 @@
+//BigInt
+// Symbol
+let foo = 42; // foo is now a number
+foo = "bar"; // foo is now a string
+foo = true; // foo is now a boolean
+
+
+
+
 console.log(typeof null)
 console.log(typeof undefined)
