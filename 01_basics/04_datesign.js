4abbf4a9d6efef1dc444dd2f9fbf83b79650193c
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-02-25 22:19:46 +0530

    javascript date method

---
+++ 01_basics/04_datesign.js
@@ -0,0 +1,6 @@
+const newDate=new Date();
+console.log("toDateString",newDate.toDateString())
+console.log("toLocaleDateString",newDate.toLocaleDateString())
+console.log("toLocaleString",newDate.toLocaleString())
+console.log("toLocaleTimeString",newDate.toLocaleTimeString())
+console.log("toJSON",newDate.toJSON())
