02617aca0fca1f0d84872ab1aab964266420855e
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-02-26 11:37:07 +0530

    push and concat

---
+++ 01_basics/05_array.js
@@ -0,0 +1,37 @@
+const arr=[1,2,3,4,5];
+
+// push: push is used  to add an element at the end of array 
+arr.push(6)
+arr.push(7)
+arr.push(8)
+// pop :pop is used to remove an element at the end of array
+arr.pop()
+
+// unshift:unshift is used to add an element at the start of array 
+arr.unshift(9)
+
+// shift:unshift is used to remove an element at the start of array 
+arr.shift()
+
+
+// console.log(arr.includes(5))
+// console.log(arr.join('-'))
+
+// slice :slice method is return a new array
+// console.log("A",arr)
+// console.log(arr.slice(1,3))
+// console.log("B",arr)
+// splice:splice is modify the original array 
+// console.log(arr.splice(1,3))
+// console.log("C",arr)
+
+// +++++++++++ Push 
+let arr2=[9,8,7,6,5]
+
+// arr.push(arr2)
+console.log(arr)
+let arrRes=arr.concat(arr2)
+console.log("asrrRes",arrRes)
+
+
+
