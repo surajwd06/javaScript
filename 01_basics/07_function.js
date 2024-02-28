6aaf24c657ac9a5cd46232a9a1f21d18364be8fd
Suraj Yadav <126961028+surajwd06@users.noreply.github.com>
2024-02-28 12:17:14 +0530

    functions

--- 01_basics/07_function.js
+++ 01_basics/07_function.js
@@ -1,8 +1,41 @@
-function syMyName(){
-    console.log("s")
-    console.log("u")
-    console.log("r")
-    console.log("a")
-    console.log("j")
+// function syMyName(){
+//     console.log("s")
+//     console.log("u")
+//     console.log("r")
+//     console.log("a")
+//     console.log("j")
+// }
+// syMyName()
+
+function addTwoNumbers(num1, num2) {
+  let result = num1 + num2;
+  return result;
 }
-syMyName()
+
+let result = addTwoNumbers(5, 4);
+// console.log(result)
+
+function calculateCardPrise(...num) {
+  let count = 0;
+  num.map((item) => {
+    return (count += item);
+  });
+  console.log(num);
+  return count;
+}
+
+// console.log(calculateCardPrise(100, 200, 300, 400, 500));
+
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+handleObject({
+    username:"suraj",
+    price:3999
+})
+
+function handleArray(array){
+console.log(`This array is ${array[0]}`)
+}
+handleArray([1,2,3,4,5])
